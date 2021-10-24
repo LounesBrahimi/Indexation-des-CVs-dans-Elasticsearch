ddeb67a5ef192ea8ea40593d7aef75e2715b4a69
Lounes Brahimi <lounesnewgs5@gmail.com>
2021-10-24 11:57:16 +0200

    cleanning

---
+++ indexation/src/test/java/com/cv/indexation/IndexationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cv.indexation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndexationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
