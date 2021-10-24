42c7289426505de99da8ee9ea49d2459b9d5d9f9
Lounes Brahimi <lounesnewgs5@gmail.com>
2021-10-24 12:34:07 +0200

    creation of index controller for creation and recreation

---
+++ indexation/src/main/java/com/cv/indexation/controller/IndexController.java
@@ -0,0 +1,24 @@
+package com.cv.indexation.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cv.indexation.service.IndexService;
+
+@RestController
+@RequestMapping("/api/index")
+public class IndexController {
+    private final IndexService service;
+
+    @Autowired
+    public IndexController(IndexService service) {
+        this.service = service;
+    }
+
+    @PostMapping("/recreate")
+    public void recreateAllIndices() {
+        service.recreateIndices(true);
+    }
+}
